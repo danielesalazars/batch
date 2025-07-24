a5e15df44eb8dd2f2868afb0a01d177d82d5e787
Daniel Salazar <daniel.esalazars@gmail.com>
2025-07-24 14:40:38 +0000

    Update user.queries.ts

--- src/users/infrastructure/repositories/data-access/sql/user.queries.ts
+++ src/users/infrastructure/repositories/data-access/sql/user.queries.ts
@@ -1,6 +1,6 @@
 export const UserQueries = {
   create: `
-    INSERT INTO [user] (first_name, last_name, email, password_hash, role, is_active) 
+    INSERT INTO [user] (first_name, last_name, email, password_hash, role, is_active)
     OUTPUT inserted.*
-    VALUES (@first_name, @last_name, @Email, @password_hash, @role, @is_active);`,
+    VALUES (@param1, @param2, @param3, @param4, @param5, @param6);`,
 };
